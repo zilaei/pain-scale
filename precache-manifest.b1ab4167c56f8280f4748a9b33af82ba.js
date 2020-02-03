92013473b00bcabd9a910a027e773f03fba2d924
Max Frederiksen <max@colesmedia.se>
2020-02-03 16:39:50 +0100

    Updates

---
+++ precache-manifest.b1ab4167c56f8280f4748a9b33af82ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4602d7e5de22f8c2425ed08ae8ba5edc",
+    "url": "/pain-scale/index.html"
+  },
+  {
+    "revision": "30f3ca611d8bc97d74ac",
+    "url": "/pain-scale/static/css/main.6b290bb0.chunk.css"
+  },
+  {
+    "revision": "381c085dcc725ccc7772",
+    "url": "/pain-scale/static/js/2.e8796287.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/pain-scale/static/js/2.e8796287.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30f3ca611d8bc97d74ac",
+    "url": "/pain-scale/static/js/main.97b497df.chunk.js"
+  },
+  {
+    "revision": "f9c94da7f798c32f90fe",
+    "url": "/pain-scale/static/js/runtime-main.290f9725.js"
+  }
+]);
